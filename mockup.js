3b2ce9aeb99614c4fa9f9b41fae3b0f672112777
sachetsingh77 <sachet@leverj.io>
2019-10-24 12:56:56 +0530

    updated

--- mockup.js
+++ mockup.js
@@ -1,20 +1,19 @@
-<script>
-    
-                                function showNavMenu(){
-                                  console.log('show Nav Menu');
-                                  var menuDivs = document.getElementsByClassName('c0198 c01105');
-                                  var menuDiv = menuDivs[0];
-                                  console.log(menuDiv.style.display);
-                                  if (menuDiv.style.display == "initial"){
-                                    menuDiv.style.display = "none";
-                                  }
-                                  else {
-                                    menuDiv.style.display = "initial";
-                                  }
-                                  console.log(menuDiv.style.display);
+<script language="javascript">
 
-                                }
-                                var mainMenu = document.getElementById("mainMenuNav");
-                                console.log('mainMenuNav ' + mainMenu.class);
-                                mainMenu.addEventListener("click", showNavMenu);
-                                </script>
+    function showNavMenu(){
+        console.log('show Nav Menu');
+        var menuDivs = document.getElementsByClassName('c0198 c01105');
+        var menuDiv = menuDivs[0];
+        console.log(menuDiv.style.display);
+        if (menuDiv.style.display == "initial"){
+            menuDiv.style.display = "none"
+        }
+        else {
+            menuDiv.style.display = "initial"
+        }
+        console.log(menuDiv.style.display);
+    }
+    var mainMenu = document.getElementById("mainMenuNav");
+    console.log('mainMenuNav ' + mainMenu.class);
+    mainMenu.addEventListener("click", showNavMenu);
+</script>
